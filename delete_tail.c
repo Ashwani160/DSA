20644b3becfcab568dc06a48cc00a05508dda35d
Ashwani160 <ashwani11002004@gmail.com>
2024-11-18 12:05:31 +0530

    first

---
+++ delete_tail.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node* prev;
+    struct node* next;
+};
+struct node* create_node(){
+    struct node* temp=(struct node*)malloc(sizeof(struct node));
+    int data;
+    printf("Enter data: ");
+    scanf("%d",&data);
+    temp->data=data;
+    temp->prev=temp;
+    temp->next=temp;
+    return temp;
+}
+struct node* create_CLL(int n){
+    struct node* head=create_node();
+    struct node*temp=head;
+    struct node* newnode;
+    for(int i=1;i<n;i++){
+        newnode=create_node();
+        newnode->next=temp->next;
+        temp->next=newnode;
+        newnode->prev=temp;
+        temp=newnode;
+    }
+    head->prev=newnode;
+    return head;
+}
+void traverse(struct node* head){
+    struct node* ptr=head;
+    do{
+        printf("%d ",ptr->data);
+        ptr=ptr->next;
+    }while(ptr!=head);
+    printf("\n");
+}
+void delete_tail(struct node* head){
+    struct node* tail=head->prev;
+    tail->prev->next=head;
+    head->prev=tail->prev;
+    free(tail);
+}
+int main(){
+    int n;
+    printf("Enter the size of list: ");
+    scanf("%d",&n);
+    struct node* head=create_CLL(n);
+    traverse(head);
+    delete_tail(head);
+    traverse(head);
+}
